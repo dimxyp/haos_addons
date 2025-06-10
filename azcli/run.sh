b35266b505e250bc8a1855f752347f5cefe8664c
dimx <xypolias.d@outlook.com>
2025-06-10 15:33:48 +0300

    ex

--- azcli/run.sh
+++ azcli/run.sh
@@ -3,17 +3,20 @@ set -e
 
 echo "Starting Azure CLI Add-on..."
 
-TENANT_ID=$(bashio::config 'tenant_id')
-CLIENT_ID=$(bashio::config 'client_id')
-CLIENT_SECRET=$(bashio::config 'client_secret')
+CONFIG_PATH="/data/options.json"
 
-bashio::log.info "Tenant ID: ${TENANT_ID}"
-bashio::log.info "Client ID: ${CLIENT_ID}"
-bashio::log.info "Client Secret: ${CLIENT_SECRET}"
+TENANT_ID=$(jq --raw-output '.tenant_id // empty' $CONFIG_PATH)
+CLIENT_ID=$(jq --raw-output '.client_id // empty' $CONFIG_PATH)
+CLIENT_SECRET=$(jq --raw-output '.client_secret // empty' $CONFIG_PATH)
+
+echo "Tenant ID: ${TENANT_ID}"
+echo "Client ID: ${CLIENT_ID}"
+echo "Client Secret: ${CLIENT_SECRET}"
+
 
 # Login with Service Principal using env vars from config.json
-#az login --service-principal --username "$CLIENT_ID" --password "$CLIENT_SECRET" --tenant "$TENANT_ID"
-#az account list --output table || echo "You may need to login using a service principal."
+az login --service-principal --username "$CLIENT_ID" --password "$CLIENT_SECRET" --tenant "$TENANT_ID"
+az account list --output table || echo "You may need to login using a service principal."
 #echo "Logged in as Service Principal, ready for commands."
 
 # Keep running to keep container alive
